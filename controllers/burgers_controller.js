e1e0b5954685f61260ee9f484b50e96b39bd80be
danman15726 <danman15726@gmail.com>
2020-01-16 17:30:49 -0600

    Added code in the config.json file for the port number, specified to 3306. Changed burgers_controller.js to sequelize code. Still need to fix the regular router.get function.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,45 +2,59 @@ const express = require("express");
 const router = express.Router();
 
 
-const burger = require("../models/burger.js");
+const db = require("../models/burger.js");
 
 
 
 router.get("/", function(req, res){
-    burger.all(function(data){
-        const hbsObject = {
-            burgers: data
-        };
-        console.log(hbsObject);
-        res.render("index", hbsObject);
+    db.burgers.find({}).then(function(dbburger){
+        
+        res.json(dbburger);
     });
+
+
+
+
+
+
+
 });
+
 
 router.post("/api/burgers", function(req, res){
-    burger.insert(["burger_name", "devoured"], [req.body.burger, 0], function(result){
-        res.json({ id: result.insertId});
+    console.log(req.body);
+
+    const {
+
+        burger_name,
+        devoured
+    } = req.body;
+
+
+    db.burgers.create({
+
+        burger_name,
+        devoured
+    }).then(function(dbburger){
+
+        res.json(dbburger);
     });
+
+
 });
 
-router.put("/api/burgers/:id", function(req, res){
-    const condition = `id = ${req.params.id}`;
-    console.log(`condition, ${condition}`);
 
-    burger.update(
+router.put("/api/burgers/:id", function(req, res){
+    
+
+    db.burgers.update(
         {
             devoured: req.body.devoured
         },
-        condition,
-        function(result){
-            if (result.changedRows === 0){
-                return res.status(404).end();
-            }
-
-
-            res.status(200).end();
-        }
-    );
-
+    ).then(function(updated){
+        res.json(updated);
+    });
+    
 });
 
 
